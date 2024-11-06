6a87d318b5412387c648ace5b39a3fb0bf204199
NickCamara <nicholasygor@gmail.com>
2024-11-06 01:59:19 -0300

    feat/add authetication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "./_components/ui/button";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
-const Home = () => {
+const Home = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
   return (
-    <Button>Hello World!!!</Button>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName />
+    </div>
   );
 };
 
